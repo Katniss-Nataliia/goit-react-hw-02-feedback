f0267ae532c4b6d01706c4476b9e2ff9c714bd2c
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-27 23:09:35 -0600

    added code

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -1,28 +1,45 @@
 import React, {Component} from "react";
-// import ReactDOM from "react";
 
 export class FeedbackWidget extends Component {
-    state = {
-      good:0,
-      neutral:0,
-      bad:0
-    };
-  
+
+    constructor(props){
+        super(props);
+        this.state = {
+            good:0,
+            neutral:0,
+            bad:0,
+            
+        }
+        
+    }
+    handleIncrementGood = () => {
+        this.setState({good: this.state.good + 1})
+    }
+    handleIncrementNeutral = () => {
+        this.setState({neutral: this.state.neutral + 1})
+    }
+    handleIncrementBad = () => {
+        this.setState({bad: this.state.bad + 1})
+    }
+    countTotalFeedback = () => {
+        const total = [...this.state.]
+
+    }
+
+   
     render() {
-      const { good, neutral, bad } = this.state;
-      
-  
       return (
         <div>
-            <h1>Please leave feedback</h1>
-          <button type="button" onClick={event => console.log(good)}>Good{good}</button>
-          <button type="button">Neutral {neutral}</button>
-          <button type="button">Bad{bad}</button>
+          <h1>Please leave feedback</h1>
+          <button type="button" onClick={this.handleIncrementGood}>Good</button>
+          <button type="button" onClick={this.handleIncrementNeutral}>Neutral</button>
+          <button type="button" onClick={this.handleIncrementBad}>Bad</button>
         
             <h1>Statistics</h1>
-            <p>Good:{this.props.good}</p>
-            <p>Neutral:</p>
-            <p>Bad:</p>
+            <p>Good: {this.state.good}</p>
+            <p>Neutral: {this.state.neutral}</p>
+            <p>Bad: {this.state.bad}</p>
+            <p>Total: {this.total}</p>
         </div>
       );
     }
